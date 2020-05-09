9e316ca047c10bb5feda0d283ed51f9fa3d980f5
Verticus <40356161+Verticus@users.noreply.github.com>
2020-05-09 17:52:08 +0800

    Add files via upload

---
+++ aspnetapp/Controllers/HomeController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using aspnetapp.Models;
+using MySql.Data.MySqlClient;  
+using aspnetapp.Utils;
+using aspnetapp.Dao;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+
+namespace aspnetapp.Controllers
+{
+    public class HomeController : Controller
+    {
+        public string title = ".Netcore Demo For Rainbond";
+        public IActionResult Index()
+        {
+            ViewData["Title"] = title;
+            return View();
+        }
+
+        [Route("mysqldemo")]
+        public IActionResult MysqlDemo()
+        {
+            ViewData["Title"] = title;
+            ViewData["MYSQL_HOST"] = Environment.GetEnvironmentVariable("MYSQL_HOST");
+            ViewData["MYSQL_PORT"] = Environment.GetEnvironmentVariable("MYSQL_PORT");
+            ViewData["MYSQL_USER"] = Environment.GetEnvironmentVariable("MYSQL_USER");
+            ViewData["MYSQL_PASSWORD"] = Environment.GetEnvironmentVariable("MYSQL_PASSWORD");
+            ViewData["MYSQL_DATABASE"] = Environment.GetEnvironmentVariable("MYSQL_DATABASE");
+            return View();
+        }
+        [Route("apidemo")]
+        public IActionResult APIDemo()
+        {
+            ViewData["Title"] = title;
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ aspnetapp/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace aspnetapp {
+    public class Program {
+        public static void Main (string[] args) {
+            string port = "5000";
+            if (Environment.GetEnvironmentVariable("PORT") != null) {
+                port = Environment.GetEnvironmentVariable("PORT");
+            }
+            Console.WriteLine("Listing :{0}", port);
+            CreateWebHostBuilder (port, args).Build ().Run();
+            while (true)
+            {
+                Thread.Sleep(1000);
+                Console.WriteLine("Hello world! ");
+            }
+        }
+        public static IWebHostBuilder CreateWebHostBuilder (string port, string[] args) =>
+            WebHost.CreateDefaultBuilder (args)
+            .UseUrls ($"http://0.0.0.0:{port}")
+            .UseStartup<Startup> ();
+    }
+}
